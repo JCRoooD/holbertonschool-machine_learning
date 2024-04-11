844197dea93558c9ae88e8308653ab3807382419
JCRoooD <juancarlos-99@live.com>
2024-04-11 12:15:44 -0400

    Refactor matrix_shape function to use "is" instead of "==" for type comparison in 2-size_me_please.py

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python3
 def matrix_shape(matrix):
-    """calculate shape of matrix"""
+    """
+    Calculate the shape of a matrix.
+
+    Args:
+        matrix (list): The matrix to calculate the shape of.
+
+    Returns:
+        list: A list representing the shape of the matrix. Each element in the list
+              corresponds to the size of a dimension in the matrix.
+
+    Example:
+        >>> matrix = [[1, 2, 3], [4, 5, 6]]
+        >>> matrix_shape(matrix)
+        [2, 3]
+    """
     shape = []
     while type(matrix) is list:
         shape.append(len(matrix))
