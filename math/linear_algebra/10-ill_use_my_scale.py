343f470949f9a591e2bcfc176b3d0c3c9714ba43
JCRoooD <juancarlos-99@live.com>
2024-04-12 11:29:02 -0400

    Refactor np_shape function to remove unnecessary import in 10-ill_use_my_scale.py

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python34
 """Module that calculates the shape of a numpy.ndarray"""
 
-import numpy as np
-
 
 def np_shape(matrix):
     """Calculates the shape of a numpy.ndarray"""
