732a4148c7ef6fe1a49bb5af6272f4b5dc469428
JCRoooD <juancarlos-99@live.com>
2024-04-15 19:59:17 -0400

    pycodestyle fix

--- math/plotting/1-scatter.py
+++ math/plotting/1-scatter.py
@@ -2,6 +2,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def scatter():
 
     mean = [69, 0]
@@ -11,9 +12,11 @@ def scatter():
     y += 180
     plt.figure(figsize=(6.4, 4.8))
 
-    plt.scatter(x, y, color='magenta')
-    plt.xlabel('Height (in)')
-    plt.ylabel('Weight (lbs)')
-    plt.title('Men\'s Height vs Weight')
+    plt.scatter(x, y, color="magenta")
+    plt.xlabel("Height (in)")
+    plt.ylabel("Weight (lbs)")
+    plt.title("Men's Height vs Weight")
     plt.show()
+
+
 scatter()
