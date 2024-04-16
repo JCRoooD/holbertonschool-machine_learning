09e47748d33168201a4be35d1af47a2f326632ad
JCRoooD <juancarlos-99@live.com>
2024-04-15 21:31:07 -0400

    pycodestyle fix

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -3,13 +3,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def frequency():
     """Create Histogram"""
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
 
-    plt.hist(student_grades, bins=10, range=(0, 100), edgecolor='black')
+    plt.hist(student_grades, bins=10, range=(0, 100), edgecolor="black")
     plt.xticks(np.arange(0, 101, 10))
     plt.xlim(0, 100)
     plt.ylim(0, 30)
