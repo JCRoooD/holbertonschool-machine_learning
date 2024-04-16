f7885787e0a27493563d598a89e2e43a3cb68e8a
JCRoooD <juancarlos-99@live.com>
2024-04-16 15:44:58 -0400

    Add stack bar graph plotting function in 6-bars.py

---
+++ math/plotting/6-bars.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Stack graph"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+def bars():
+    """creating a stack bar graph"""
+    np.random.seed(5)
+    fruit = np.random.randint(0, 20, (4,3))
+    plt.figure(figsize=(6.4, 4.8))
+
+    plt.bar([1, 2, 3], fruit[0], color='red', label='apples', width=0.5)
+    plt.bar([1, 2, 3], fruit[1], color='yellow', bottom=fruit[0], label='bananas', width=0.5)
+    plt.bar([1, 2, 3], fruit[2], color='#ff8000', bottom=fruit[0]+fruit[1], label='oranges', width=0.5)
+    plt.bar([1, 2, 3], fruit[3], color='#ffe5b4', bottom=fruit[0]+fruit[1]+fruit[2], label='peaches', width=0.5)
+    plt.ylabel('Quantity of Fruit')
+    plt.ylim(0, 80)
+    plt.title('Number of Fruit per Person')
+
+    plt.xticks([1, 2, 3], ['Farrah', 'Fred', 'Felicia'])
+    plt.legend()
+    plt.show()
