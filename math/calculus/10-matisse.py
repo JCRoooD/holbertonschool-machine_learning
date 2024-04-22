cebec2b93c62fa972dfd2352952be874b4521de6
JCRoooD <juancarlos-99@live.com>
2024-04-21 22:32:48 -0400

    Add poly_derivative function and test in math/calculus directory

---
+++ math/calculus/10-matisse.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""poly derivative"""
+
+
+def poly_derivative(poly):
+    """function that calculates the derivative of a polynomial"""
+    if not isinstance(poly, list) or len(poly) == 0:
+        return None
+
+    derivative = [poly[i] * i for i in range(1, len(poly))]
+
+    return derivative
