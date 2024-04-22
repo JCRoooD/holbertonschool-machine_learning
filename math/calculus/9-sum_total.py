3644b1a996c87965e5908d712967faab150adec3
JCRoooD <juancarlos-99@live.com>
2024-04-21 23:00:04 -0400

    Fix condition in summation_i_squared function to handle non-integer values of n

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -4,7 +4,7 @@
 
 def summation_i_squared(n):
     """function that sums in sigma notation"""
-    if not isinstance(n, int) and n <= 0:
+    if not isinstance(n, int) or n <= 0:
         return None
 
     squared_sum = (n * (n + 1) * (2 * n + 1)) // 6
