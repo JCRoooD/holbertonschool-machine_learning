5ca27709430e22bb9fc890edca3496ed2a0d5333
JCRoooD <juancarlos-99@live.com>
2024-04-30 16:27:15 -0400

    pycodestyle fix

--- supervised_learning/classification/1-neuron.py
+++ supervised_learning/classification/1-neuron.py
@@ -5,29 +5,27 @@ import numpy as np
 
 class Neuron:
     """defining class Neuron"""
+
     def __init__(self, nx):
         """constructor class for instance of Neuron"""
         if isinstance(nx, int) is False:
-            raise TypeError ("nx must be a integer")
+            raise TypeError("nx must be a integer")
         if nx < 1:
-            raise ValueError ("nx must be a positive integer")
+            raise ValueError("nx must be a positive integer")
 
         self.__W = np.random.randn(nx).reshape(1, nx)
         self.__b = 0
         self.__A = 0
-
 
     @property
     def W(self):
         """Getter func"""
         return self.__W
 
-
     @property
     def b(self):
         """Getter func"""
         return self.__b
-
 
     @property
     def A(self):
