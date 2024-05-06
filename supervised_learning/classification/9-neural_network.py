556c21408195a049084febebdd8829edd5d8fe88
JCRoooD <juancarlos-99@live.com>
2024-05-06 13:02:31 -0400

    Refactor neural network class initialization with private instances

--- supervised_learning/classification/9-neural_network.py
+++ supervised_learning/classification/9-neural_network.py
@@ -29,36 +29,30 @@ class NeuralNetwork:
         self.__b2 = 0
         self.__A2 = 0
 
-
     @property
     def W1(self):
         """Getter for weights"""
         return self.__W1
-
 
     @property
     def b1(self):
         """Getter for bias"""
         return self.__b1
 
-
     @property
     def A1(self):
         """Getter for activated output"""
         return self.__A1
-
 
     @property
     def W2(self):
         """Getter for weights"""
         return self.__W2
 
-
     @property
     def b2(self):
         """Getter for bias"""
         return self.__b2
-
 
     @property
     def A2(self):
