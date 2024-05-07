255d7fe0f73c7e4aad15ba0c1976a5659e60af19
JCRoooD <juancarlos-99@live.com>
2024-05-07 14:20:25 -0400

    Refactor neural network class initialization with private instances

---
+++ supervised_learning/classification/10-neural_network.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+"""based on 9-neural_network.py"""
+import numpy as np
+
+
+class NeuralNetwork:
+    """Initializes a neural network with
+    one hidden layer for binary classification"""
+    def __init__(self, nx, nodes):
+        """constructor class"""
+        # Validate input types and values
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if not isinstance(nodes, int):
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+
+        # Initialize hidden layer weights, bias, and output
+        self.__W1 = np.random.randn(nodes, nx)
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+
+        # Initialize output neuron weights, bias, and output
+        self.__W2 = np.random.randn(1, nodes)
+        self.__b2 = 0
+        self.__A2 = 0
+
+    @property
+    def W1(self):
+        """Getter for weights"""
+        return self.__W1
+
+    @property
+    def b1(self):
+        """Getter for bias"""
+        return self.__b1
+
+    @property
+    def A1(self):
+        """Getter for activated output"""
+        return self.__A1
+
+    @property
+    def W2(self):
+        """Getter for weights"""
+        return self.__W2
+
+    @property
+    def b2(self):
+        """Getter for bias"""
+        return self.__b2
+
+    @property
+    def A2(self):
+        """Getter for activated output"""
+        return self.__A2
+
+    def forward_prop(self, X):
+        """Calculates the forward propagation of the neural network"""
+        # Z1 is the dot product of weights and
+        #           input data plus bias for the hidden layer
+        Z1 = np.matmul(self.W1, X) + self.b1
+        # Apply sigmoid activation function
+        #           to Z1 to get A1 (output of hidden layer)
+        self.__A1 = 1 / (1 + np.exp(-Z1))
+
+        # Z2 is the dot product of weights
+        # and A1 plus bias for the output layer
+        Z2 = np.matmul(self.W2, self.__A1) + self.b2
+        # Apply sigmoid activation function
+        #           to Z2 to get A2 (final output)
+        self.__A2 = 1 / (1 + np.exp(-Z2))
+
+        # Return A1 and A2
+        return self.__A1, self.__A2
