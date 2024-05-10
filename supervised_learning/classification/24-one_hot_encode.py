294443ce831867b0fa59efbc83245d16b1e05273
JCRoooD <juancarlos-99@live.com>
2024-05-10 11:13:56 -0400

    fix

--- supervised_learning/classification/24-one_hot_encode.py
+++ supervised_learning/classification/24-one_hot_encode.py
@@ -5,7 +5,17 @@ import numpy as np
 
 def one_hot_encode(Y, classes):
     """converts a numeric label vector into a one-hot matrix"""
+    if not isinstance(Y, np.ndarray) or not isinstance(classes, int):
+        return None
+    if len(Y) == 0:
+        return None
+    if classes <= np.max(Y):
+        return None
     try:
-        return np.eye(classes)[Y].T
+        matrix = np.zeros((classes, len(Y)))
+
+        matrix[Y, np.arange(len(Y))] = 1
+        return matrix
+
     except Exception:
         return None
