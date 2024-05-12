abf272d9cab4fd7a23b2a6dc118af5d1e761032b
JCRoooD <juancarlos-99@live.com>
2024-05-12 19:02:35 -0400

    trying to fix

--- supervised_learning/tensorflow/1-create_layer.py
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -1,19 +1,16 @@
 #!/usr/bin/env python3
-"""This module defines a function  create a neuron layer
-"""
+"""Module for create_layer function"""
+
 import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
 
 
 def create_layer(prev, n, activation):
-    """
-Creates a neural network layer with weights, biases, and an activation function.
-"""
-# Initialize weights with He et. al method
-initializer = tf.keras.initializers.VarianceScaling(mode='fan_avg')
+    """Create layer function"""
+    initializer = tf.keras.initializers.VarianceScaling(mode='fan_avg')
 
-# Define weights and biases
-weights = tf.Variable(initializer(shape=(int(prev.shape[1]), n)), name='weights')
-biases = tf.Variable(tf.zeros([n]), name='biases')
+    layer = tf.layers.Dense(units=n, activation=activation,
+                            kernel_initializer=initializer,
+                            name="layer")
 
-# Compute weighted sum of inputs and apply activation function
-layer = tf.add(tf.matmul(prev, weights), biases)
+    return layer(prev)
