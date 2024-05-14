ae4587f9bc6ece90b3ab8d09680b8dfd5e429fe2
JCRoooD <juancarlos-99@live.com>
2024-05-14 14:53:56 -0400

    keep fixing function

--- supervised_learning/optimization/3-mini_batch.py
+++ supervised_learning/optimization/3-mini_batch.py
@@ -18,7 +18,7 @@ def create_mini_batches(X, Y, batch_size):
         Y_batch = Y[i * batch_size: i * batch_size + batch_size, :]
         mini_batches.append((X_batch, Y_batch))
 
-    if m * batch_size < m:
+    if m % batch_size != 0:
         X_batch = X[complete_batches * batch_size: m, :]
         Y_batch = Y[complete_batches * batch_size: m, :]
         mini_batches.append((X_batch, Y_batch))
