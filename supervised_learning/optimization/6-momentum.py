0a304069629a34e6b07a12f4d06758966ab50c4d
JCRoooD <juancarlos-99@live.com>
2024-05-14 23:08:58 -0400

    create momentum

---
+++ supervised_learning/optimization/6-momentum.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Momentum optimization algorithm"""
+import numpy as np
+import tensorflow as tf
+
+def create_momentum_op(loss, alpha, beta1):
+    """creates the training operation for a neural network in tensorflow
+    using the gradient descent with momentum optimization algorithm:
+    loss is the loss of the network
+    alpha is the learning rate
+    beta1 is the momentum weight
+    Returns: the momentum optimization operation"""
+    return tf.train.MomentumOptimizer(alpha, beta1).minimize(loss)
