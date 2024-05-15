d87006f16f33371292d267bb8af903b2a7d8e3e9
JCRoooD <juancarlos-99@live.com>
2024-05-14 23:14:32 -0400

    typo deleted unused parameter

--- supervised_learning/optimization/6-momentum.py
+++ supervised_learning/optimization/6-momentum.py
@@ -3,7 +3,7 @@
 import numpy as np
 import tensorflow as tf
 
-def create_momentum_op(loss, alpha, beta1):
+def create_momentum_op(alpha, beta1):
     """creates the training operation for a neural network in tensorflow
     using the gradient descent with momentum optimization algorithm:
     loss is the loss of the network
