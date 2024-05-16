41e8a5a480acc5dce80663842e25378a65b99df5
JCRoooD <juancarlos-99@live.com>
2024-05-16 13:33:12 -0400

    typo in function name

--- supervised_learning/optimization/10-Adam.py
+++ supervised_learning/optimization/10-Adam.py
@@ -3,7 +3,7 @@
 import tensorflow as tf
 
 
-def create_Adam_op(loss, alpha, beta1, beta2, epsilon):
+def create_Adam_op(alpha, beta1, beta2, epsilon):
     """creates the training operation for a neural network in tensorflow
     using the Adam optimization algorithm:
     loss is the loss of the network
