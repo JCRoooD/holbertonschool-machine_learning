e62205f69c955e5141dd3489273122d5521ef97e
JCRoooD <juancarlos-99@live.com>
2024-05-16 14:36:43 -0400

    pycodestyle

--- supervised_learning/optimization/13-batch_norm.py
+++ supervised_learning/optimization/13-batch_norm.py
@@ -8,11 +8,11 @@ import numpy as np
 
 def batch_norm(Z, gamma, beta, epsilon):
     """
-    Normalizes an unactivated output (Z) of a neural network using batch 
+    Normalizes an unactivated output (Z) of a neural network using batch
     normalization.
 
     Parameters:
-    Z (numpy.ndarray): The unactivated output of a neural network. 
+    Z (numpy.ndarray): The unactivated output of a neural network.
     gamma (numpy.ndarray): A numpy array used for
     element-wise scaling of the normalized Z.
     beta (numpy.ndarray): A numpy array used for
@@ -22,8 +22,8 @@ def batch_norm(Z, gamma, beta, epsilon):
     Returns:
     Z_tilda (numpy.ndarray): The batch-normalized output.
 
-    Batch normalization is a technique for improving the speed, performance, 
-    and stability of artificial neural networks. It is used to normalize the 
+    Batch normalization is a technique for improving the speed, performance,
+    and stability of artificial neural networks. It is used to normalize the
     input layer by adjusting and scaling the activations.
     """
     # Calculate the mean of Z
