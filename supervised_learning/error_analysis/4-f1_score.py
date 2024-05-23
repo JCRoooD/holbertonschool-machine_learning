b92f084c8475066f252f9367b12a099f52667e35
JCRoooD <juancarlos-99@live.com>
2024-05-23 16:33:53 -0400

    added blank line

--- supervised_learning/error_analysis/4-f1_score.py
+++ supervised_learning/error_analysis/4-f1_score.py
@@ -4,6 +4,7 @@ import numpy as np
 sensitivity = __import__('1-sensitivity').sensitivity
 precision = __import__('2-precision').precision
 
+
 def f1_score(confusion):
     """ calculates the F1 score of a confusion matrix """
     precision = np.diag(confusion) / np.sum(confusion, axis=0)
