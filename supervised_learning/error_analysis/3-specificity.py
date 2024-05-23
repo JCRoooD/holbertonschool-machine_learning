c24519f3d16bfda30ff08c4f6e11233d7b06a142
JCRoooD <juancarlos-99@live.com>
2024-05-23 16:29:58 -0400

    refractor

--- supervised_learning/error_analysis/3-specificity.py
+++ supervised_learning/error_analysis/3-specificity.py
@@ -5,4 +5,18 @@ import numpy as np
 
 def specificity(confusion):
     """ calculates the specificity for each class in a confusion matrix """
-    return np.diag(confusion) / np.sum(confusion, axis=0)
+    total = np.sum(confusion)
+    # We can get the True Positives (TP) by summing the diagonal
+    TP = np.diag(confusion)
+    # Sum each row (actual positives)
+    actual_positives = np.sum(confusion, axis=1)
+    # Sum each column (predicted positives)
+    predicted_positives = np.sum(confusion, axis=0)
+    # True Negatives (TN) are all values that are not in the
+    # actual or predicted
+    TN = total - (actual_positives + predicted_positives - TP)
+    # False Positives (FP) are the values that are not in the actual
+    # but in the predicted
+    FP = predicted_positives - TP
+    # Calculate the specificity for each class
+    return TN / (TN + FP)
