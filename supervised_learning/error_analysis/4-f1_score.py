7f709b21734e2162823c259d4e568cb0554d7c9c
JCRoooD <juancarlos-99@live.com>
2024-05-23 16:31:57 -0400

    refractor function

--- supervised_learning/error_analysis/4-f1_score.py
+++ supervised_learning/error_analysis/4-f1_score.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """ F1 Score """
 import numpy as np
-
+sensitivity = __import__('1-sensitivity').sensitivity
+precision = __import__('2-precision').precision
 
 def f1_score(confusion):
     """ calculates the F1 score of a confusion matrix """
