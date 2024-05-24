6c1643e373e389a7eee95cefc736e0a937dfeeee
JCRoooD <juancarlos-99@live.com>
2024-05-24 17:57:44 -0400

    3. Create a Layer with L2 Regularization

---
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" L2 Regularization Cost """
+import tensorflow as tf
+
+
+def l2_reg_create_layer(prev, n, activation, lambtha):
+    """ creates a tensorflow layer that includes L2 regularization
+        prev: tensor containing the output of the previous layer
+        n: number of nodes the new layer should contain
+        activation: activation function that should be used on the layer
+        lambtha: L2 regularization parameter
+        Returns: output of the new layer
+    """
+    init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    reg = tf.contrib.layers.l2_regularizer(lambtha)
+    layer = tf.layers.Dense(n, activation, kernel_initializer=init,
+                            kernel_regularizer=reg)
+    return layer(prev)
