93c7bee5c640c3fa79c04e6b419a803cf617d769
JCRoooD <juancarlos-99@live.com>
2024-05-24 18:05:04 -0400

    refractor fix

--- supervised_learning/regularization/2-l2_reg_cost.py
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -9,4 +9,6 @@ def l2_reg_cost(cost, model):
         model: a Keras model that includes layers with L2 regularization
         Returns: the cost of the network accounting for L2 regularization
     """
-    return cost + tf.reduce_sum(model.losses)
+    reg_losses = tf.add_n(model.losses)
+    total_cost = cost + reg_losses
+    return total_cost
