2e7f3e8ce79bdc8f59bfe7680a48957506e51af7
JCRoooD <juancarlos-99@live.com>
2024-05-28 19:59:02 -0400

    reafractor task 1

--- supervised_learning/keras/1-input.py
+++ supervised_learning/keras/1-input.py
@@ -6,13 +6,17 @@ import tensorflow.keras as K
 
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
-    """ 
+    """
     Returns: the keras model
     """
     inputs = K.Input(shape=(nx,))
     L2 = K.regularizers.l2(lambtha)
-    model = K.layers.Dense(layers[0], activation=activations[0], kernel_regularizer=L2)(inputs)
+    model = K.layers.Dense(layers[0], activation=activations[0], kernel_regularizer=L2)(
+        inputs
+    )
     for i in range(1, len(layers)):
         model = K.layers.Dropout(1 - keep_prob)(model)
-        model = K.layers.Dense(layers[i], activation=activations[i], kernel_regularizer=L2)(model)
+        model = K.layers.Dense(
+            layers[i], activation=activations[i], kernel_regularizer=L2
+        )(model)
     return K.Model(inputs=inputs, outputs=model)
