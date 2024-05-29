5887169f10e4e7011f77349737c5edff197f6e37
JCRoooD <juancarlos-99@live.com>
2024-05-29 14:03:43 -0400

    refractor actual import

--- supervised_learning/keras/3-one_hot.py
+++ supervised_learning/keras/3-one_hot.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 """ converting labels to one-hot matrix """
-import numpy as np
+import tensorflow.keras as K
 
 
 def one_hot(labels, classes=None):
     """ converts a label vector into a one-hot matrix """
-    if classes is None:
-        classes = np.max(labels) + 1
-    return np.eye(classes)[labels]
+    return K.utils.to_categorical(labels, classes)
+
