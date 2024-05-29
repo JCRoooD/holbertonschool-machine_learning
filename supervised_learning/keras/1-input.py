6aa8d2182b8e91ce0f8f72e5e5cd833c7664f53d
JCRoooD <juancarlos-99@live.com>
2024-05-29 11:35:18 -0400

    fixed pycodestyle

--- supervised_learning/keras/1-input.py
+++ supervised_learning/keras/1-input.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" 
+"""
 that builds a neural network with the Keras library:
 """
 import tensorflow.keras as K
@@ -11,12 +11,12 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     """
     inputs = K.Input(shape=(nx,))
     L2 = K.regularizers.l2(lambtha)
-    model = K.layers.Dense(layers[0], activation=activations[0], kernel_regularizer=L2)(
-        inputs
-    )
+    model = K.layers.Dense(layers[0],
+                           activation=activations[0],
+                           kernel_regularizer=L2)(inputs)
     for i in range(1, len(layers)):
         model = K.layers.Dropout(1 - keep_prob)(model)
-        model = K.layers.Dense(
-            layers[i], activation=activations[i], kernel_regularizer=L2
-        )(model)
+        model = K.layers.Dense(layers[i],
+                               activation=activations[i],
+                               kernel_regularizer=L2)(model)
     return K.Model(inputs=inputs, outputs=model)
