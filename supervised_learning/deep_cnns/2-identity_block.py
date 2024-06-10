433989a628e75868de715bb556bb74b9e7673156
JCRoooD <juancarlos-99@live.com>
2024-06-10 14:18:49 -0400

    fix identity block

--- supervised_learning/deep_cnns/2-identity_block.py
+++ supervised_learning/deep_cnns/2-identity_block.py
@@ -13,23 +13,35 @@ def identity_block(A_prev, filters):
         kernel_size=1,
         padding='same',
         activation='relu',
-        kernel_initializer='he_normal'
+        kernel_initializer=K.initializers.he_normal(seed=0)
     )(A_prev)
+
+    conv1 = K.layers.BatchNormalization(axis=3)(conv1)
+    conv1 = K.layers.Activation('relu')(conv1)
 
     conv2 = K.layers.Conv2D(
         filters=F3,
         kernel_size=3,
         padding='same',
         activation='relu',
-        kernel_initializer='he_normal'
+        kernel_initializer=K.initializers.he_normal(seed=0)
     )(conv1)
+
+    conv2 = K.layers.BatchNormalization(axis=3)(conv2)
+    conv2 = K.layers.Activation('relu')(conv2)
 
     conv3 = K.layers.Conv2D(
         filters=F12,
         kernel_size=1,
         padding='same',
         activation='relu',
-        kernel_initializer='he_normal'
+        kernel_initializer=K.initializers.he_normal(seed=0)
     )(conv2)
 
-    return K.layers.add([conv3, A_prev])
+    conv3 = K.layers.BatchNormalization(axis=3)(conv3)
+    conv3 = K.layers.Activation('relu')(conv3)
+
+    output = K.layers.Add()([conv3, A_prev])
+    output = K.layers.Activation('relu')(output)
+
+    return output
