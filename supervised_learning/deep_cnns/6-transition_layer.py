a811e1ef54aef1d6d4cfab7096e4fad842dfcf21
JCRoooD <juancarlos-99@live.com>
2024-06-11 15:37:17 -0400

    pycodestyle

--- supervised_learning/deep_cnns/6-transition_layer.py
+++ supervised_learning/deep_cnns/6-transition_layer.py
@@ -10,8 +10,8 @@ def transition_layer(X, nb_filters, compression):
     X = K.layers.Activation('relu')(X)
     nb_filters = int(nb_filters * compression)
     X = K.layers.Conv2D(nb_filters, (1, 1),
-                         padding='same',
-                         kernel_initializer=K.initializers.he_normal(
+                        padding='same',
+                        kernel_initializer=K.initializers.he_normal(
                              seed=0))(X)
     X = K.layers.AveragePooling2D((2, 2), strides=(2, 2))(X)
 
