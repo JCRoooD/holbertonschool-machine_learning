1359ec4a83560f3838c566b261904be431e2b78b
JCRoooD <juancarlos-99@live.com>
2024-06-11 15:05:19 -0400

    fix projection block

--- supervised_learning/deep_cnns/3-projection_block.py
+++ supervised_learning/deep_cnns/3-projection_block.py
@@ -9,52 +9,40 @@ def projection_block(A_prev, filters, s=2):
     Deep Residual Learning for Image Recognition (2015)"""
     F11, F3, F12 = filters
 
-    x_shortcut = A_prev
+    # Save the input value for the shortcut
+    X_shortcut = A_prev
 
     # First component of main path
-    X = K.layers.Conv2D(
-        filters=F11,
-        kernel_size=1,
-        strides=s,
-        padding='valid',
-        kernel_initializer='he_normal'
-    )(A_prev)
-    X = K.layers.BatchNormalization()(X)
+    X = K.layers.Conv2D(filters=F11, kernel_size=(1, 1), strides=(s, s),
+                        padding='valid',
+                        kernel_initializer=K.initializers.he_normal(seed=0)
+                        )(A_prev)
+    X = K.layers.BatchNormalization(axis=3)(X)
     X = K.layers.Activation('relu')(X)
 
     # Second component of main path
-    X = K.layers.Conv2D(
-        filters=F3,
-        kernel_size=3,
-        strides=1,
-        padding='same',
-        kernel_initializer='he_normal'
-    )(X)
-    X = K.layers.BatchNormalization()(X)
+    X = K.layers.Conv2D(filters=F3, kernel_size=(3, 3), strides=(1, 1),
+                        padding='same',
+                        kernel_initializer=K.initializers.he_normal(seed=0))(X)
+    X = K.layers.BatchNormalization(axis=3)(X)
     X = K.layers.Activation('relu')(X)
 
     # Third component of main path
-    X = K.layers.Conv2D(
-        filters=F12,
-        kernel_size=1,
-        strides=1,
-        padding='valid',
-        kernel_initializer='he_normal'
-    )(X)
-    X = K.layers.BatchNormalization()(X)
+    X = K.layers.Conv2D(filters=F12, kernel_size=(1, 1), strides=(1, 1),
+                        padding='valid',
+                        kernel_initializer=K.initializers.he_normal(seed=0))(X)
+    X = K.layers.BatchNormalization(axis=3)(X)
 
     # Shortcut path
-    x_shortcut = K.layers.Conv2D(
-        filters=F12,
-        kernel_size=1,
-        strides=s,
-        padding='valid',
-        kernel_initializer='he_normal'
-    )(x_shortcut)
-    x_shortcut = K.layers.BatchNormalization()(x_shortcut)
+    X_shortcut = K.layers.Conv2D(filters=F12,
+                                 kernel_size=(1, 1), strides=(s, s),
+                                 padding='valid',
+                                 kernel_initializer=K.initializers.he_normal(
+                                     seed=0))(X_shortcut)
+    X_shortcut = K.layers.BatchNormalization(axis=3)(X_shortcut)
 
-    # Add shortcut value to main path
-    X = K.layers.Add()([X, x_shortcut])
+    # Add shortcut value to main path, and pass it through a RELU activation
+    X = K.layers.Add()([X, X_shortcut])
     X = K.layers.Activation('relu')(X)
 
     return X
