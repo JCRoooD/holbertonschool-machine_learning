ff327931872f8812a90fa592319f44a698e566de
JCRoooD <juancarlos-99@live.com>
2024-06-11 15:02:16 -0400

    added shortcut path

--- supervised_learning/deep_cnns/3-projection_block.py
+++ supervised_learning/deep_cnns/3-projection_block.py
@@ -9,25 +9,20 @@ def projection_block(A_prev, filters, s=2):
     Deep Residual Learning for Image Recognition (2015)"""
     F11, F3, F12 = filters
 
-    x_shortcut = K.layers.Conv2D(
-        filters=F12,
-        kernel_size=1,
-        strides=s,
-        padding='same',
-        kernel_initializer='he_normal'
-    )(A_prev)
-    x_shortcut = K.layers.BatchNormalization()(x_shortcut)
+    x_shortcut = A_prev
 
+    # First component of main path
     X = K.layers.Conv2D(
         filters=F11,
         kernel_size=1,
         strides=s,
-        padding='same',
+        padding='valid',
         kernel_initializer='he_normal'
     )(A_prev)
     X = K.layers.BatchNormalization()(X)
     X = K.layers.Activation('relu')(X)
 
+    # Second component of main path
     X = K.layers.Conv2D(
         filters=F3,
         kernel_size=3,
@@ -38,15 +33,27 @@ def projection_block(A_prev, filters, s=2):
     X = K.layers.BatchNormalization()(X)
     X = K.layers.Activation('relu')(X)
 
+    # Third component of main path
     X = K.layers.Conv2D(
         filters=F12,
         kernel_size=1,
         strides=1,
-        padding='same',
+        padding='valid',
         kernel_initializer='he_normal'
     )(X)
     X = K.layers.BatchNormalization()(X)
 
+    # Shortcut path
+    x_shortcut = K.layers.Conv2D(
+        filters=F12,
+        kernel_size=1,
+        strides=s,
+        padding='valid',
+        kernel_initializer='he_normal'
+    )(x_shortcut)
+    x_shortcut = K.layers.BatchNormalization()(x_shortcut)
+
+    # Add shortcut value to main path
     X = K.layers.Add()([X, x_shortcut])
     X = K.layers.Activation('relu')(X)
 
