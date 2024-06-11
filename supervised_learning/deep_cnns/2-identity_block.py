e8699d88d52b65d498bfe900d855042344ffe209
JCRoooD <juancarlos-99@live.com>
2024-06-11 14:29:48 -0400

    fix identity block

--- supervised_learning/deep_cnns/2-identity_block.py
+++ supervised_learning/deep_cnns/2-identity_block.py
@@ -8,40 +8,40 @@ def identity_block(A_prev, filters):
     Deep Residual Learning for Image Recognition (2015)"""
     F11, F3, F12 = filters
 
-    conv1 = K.layers.Conv2D(
+    #input value for the shortcut
+    X_shortcut = A_prev
+
+    X = K.layers.Conv2D(
         filters=F11,
         kernel_size=1,
         padding='same',
-        activation='linear',
-        kernel_initializer=K.initializers.he_normal(seed=None)
+        kernel_initializer='he_normal'
     )(A_prev)
 
-    conv1 = K.layers.BatchNormalization(axis=3)(conv1)
-    conv1 = K.layers.Activation('relu')(conv1)
+    X = K.layers.BatchNormalization()(X)
+    X = K.layers.Activation('relu')(X)
 
-    conv2 = K.layers.Conv2D(
+    X = K.layers.Conv2D(
         filters=F3,
         kernel_size=3,
         padding='same',
-        activation='linear',
-        kernel_initializer=K.initializers.he_normal(seed=None)
-    )(conv1)
+        kernel_initializer='he_normal'
+    )(X)
 
-    conv2 = K.layers.BatchNormalization(axis=3)(conv2)
-    conv2 = K.layers.Activation('relu')(conv2)
+    X = K.layers.BatchNormalization()(X)
+    X = K.layers.Activation('relu')(X)
 
-    conv3 = K.layers.Conv2D(
+    X = K.layers.Conv2D(
         filters=F12,
         kernel_size=1,
         padding='same',
-        activation='linear',
-        kernel_initializer=K.initializers.he_normal(seed=None)
-    )(conv2)
+        kernel_initializer='he_normal'
+    )(X)
 
-    conv3 = K.layers.BatchNormalization(axis=3)(conv3)
+    X = K.layers.BatchNormalization()(X)
 
-    output = K.layers.Add()([conv3, A_prev])
+    X = K.layers.Add()([X, X_shortcut])
 
-    output = K.layers.Activation('relu')(output)
+    X = K.layers.Activation('relu')(X)
 
-    return output
+    return X
