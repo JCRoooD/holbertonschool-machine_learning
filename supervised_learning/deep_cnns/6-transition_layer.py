af53035ef425930caa6a83c5b2a6a42a2af1bc6e
JCRoooD <juancarlos-99@live.com>
2024-06-11 15:34:33 -0400

    transition layer fix

--- supervised_learning/deep_cnns/6-transition_layer.py
+++ supervised_learning/deep_cnns/6-transition_layer.py
@@ -4,14 +4,15 @@ from tensorflow import keras as K
 
 
 def transition_layer(X, nb_filters, compression):
-    """builds a transition layer"""
-    init = K.initializers.he_normal()
-    batch_norm = K.layers.BatchNormalization()(X)
-    activation = K.layers.Activation('relu')(batch_norm)
-    conv = K.layers.Conv2D(int(nb_filters * compression),
-                           kernel_size=(1, 1),
-                           padding='same',
-                           kernel_initializer=init)(activation)
-    avg_pool = K.layers.AveragePooling2D(pool_size=(2, 2),
-                                         strides=(2, 2))(conv)
-    return avg_pool, int(nb_filters * compression)
+    """Builds a transition layer as described in Densely Connected
+    Convolutional Networks"""
+    X = K.layers.BatchNormalization()(X)
+    X = K.layers.Activation('relu')(X)
+    nb_filters = int(nb_filters * compression)
+    X = K.layers.Conv2D(nb_filters, (1, 1),
+                         padding='same',
+                         kernel_initializer=K.initializers.he_normal(
+                             seed=None))(X)
+    X = K.layers.AveragePooling2D((2, 2), strides=(2, 2))(X)
+
+    return X, nb_filters
