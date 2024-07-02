91d17c1b6c3ee053d361fdca8a693260eecd4803
JCRoooD <juancarlos-99@live.com>
2024-07-01 20:35:09 -0400

    errors fix

--- supervised_learning/object_detection/0-yolo.py
+++ supervised_learning/object_detection/0-yolo.py
@@ -28,7 +28,7 @@ class Yolo:
         """
         self.model = K.models.load_model(model_path)
         with open(classes_path, 'r') as f:
-            self.class_names = f.read().split('\n')
+            self.class_names = f.read().splitlines()
         self.class_t = class_t
         self.nms_t = nms_t
         self.anchors = anchors
