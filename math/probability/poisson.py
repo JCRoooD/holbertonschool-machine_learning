dfe4f2de990a789e7e0d5f6dd0312a1e1642af8d
JCRoooD <juancarlos-99@live.com>
2024-07-22 19:55:37 -0400

    poisson 2

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -19,3 +19,31 @@ class Poisson:
             if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
             self.lambtha = float(lambtha)
+
+    def pmf(self, k):
+        """
+        Calculates the value of the PMF for a given number of “successes”
+        Calculates the value of the PMF for a given number of “successes”
+        Args:
+            k (int): The number of “successes”
+            Returns:
+        """ 
+        if not isinstance(k, int):
+                k = int(k)
+
+        # The PMF is 0 for negative values of k, as negative
+        # successes are not possible.
+        if k < 0:
+            return 0
+
+        # Calculate k factorial (k!) as it's required for the
+        # PMF formula.
+        factorial = 1
+        for i in range(1, k + 1):
+            factorial *= i
+
+        # Calculate and return the PMF using the formula
+        # P(k; lambda) = (e^-lambda) * (lambda^k) / k!
+        # This formula represents the probability of
+        # observing exactly k successes.
+        return ((e ** -self.lambtha) * (self.lambtha ** k)) / factorial
