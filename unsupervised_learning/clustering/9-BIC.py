72dd2eb6a041e6bc7556be87de67d147fda2d08f
JCRoooD <juancarlos-99@live.com>
2024-08-04 21:44:28 -0400

    fix pycodestyle

--- unsupervised_learning/clustering/9-BIC.py
+++ unsupervised_learning/clustering/9-BIC.py
@@ -20,31 +20,51 @@ def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
         l: numpy.ndarray of shape (kmax - kmin + 1), log likelihood for each cluster size tested
         b: numpy.ndarray of shape (kmax - kmin + 1), BIC value for each cluster size tested
     """
-    if kmax is None:
-        kmax = X.shape[0]
+     if not isinstance(X, np.ndarray) or X.ndim != 2:
+        return None, None, None, None
+    if not isinstance(kmin, int) or kmin <= 0 or X.shape[0] <= kmin:
+        return None, None, None, None
+    if not isinstance(kmax, int) or kmax <= 0 or X.shape[0] <= kmax:
+        return None, None, None, None
+    if not isinstance(iterations, int) or iterations <= 0:
+        return None, None, None, None
+    if not isinstance(tol, float) or tol < 0:
+        return None, None, None, None
+    if not isinstance(verbose, bool):
+        return None, None, None, None
+    if kmin >= kmax:
+        return None, None, None, None
 
+    # Step 2: Extract the shape from X
     n, d = X.shape
-    l = []
-    b = []
-    best_k = None
-    best_result = None
-    best_bic = float('inf')
 
-    for k in range(kmin, kmax + 1):
-        pi, m, S, g, log_likelihood = expectation_maximization(X, k, iterations, tol, verbose)
-        if pi is None or m is None or S is None or g is None or log_likelihood is None:
+    # Step 3: Initialize the variables
+    likelihoods = []
+    pis = []
+    ms = []
+    Ss = []
+    bys = []
+
+    # Step 4: Perform the EM algorithm fr each cluster size
+    for i in range(kmin, kmax + 1):
+        pi, m, S, g, likelihood = expectation_maximization(
+            X, i, iterations, tol, verbose)
+        if pi is None:
             return None, None, None, None
+        pis.append(pi)
+        ms.append(m)
+        Ss.append(S)
+        likelihoods.append(likelihood)
 
-        l.append(log_likelihood)
+        # Calculate the BIC
+        p = (i * d * (d + 1) / 2) + (d * i) + (i - 1)
+        bic = p * np.log(n) - 2 * likelihood
+        bys.append(bic)
 
-        # Calculate the number of parameters
-        p = k * d + k * d * (d + 1) / 2 + k - 1
-        bic = p * np.log(n) - 2 * log_likelihood
-        b.append(bic)
+    # Step 5: Find the best number of clusters
+    likelihoods = np.array(likelihoods)
+    bys = np.array(bys)
+    best_k = np.argmin(bys)
+    best_result = (pis[best_k], ms[best_k], Ss[best_k])
 
-        if bic < best_bic:
-            best_bic = bic
-            best_k = k
-            best_result = (pi, m, S)
-
-    return best_k, best_result, np.array(l), np.array(b)
+    return best_k+1, best_result, likelihoods, bys
