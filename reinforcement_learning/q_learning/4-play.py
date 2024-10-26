331ff88c38098aafadc1ad28e071fc1b2769abb9
JCRoooD <juancarlos-99@live.com>
2024-10-26 16:05:40 -0400

    error

--- reinforcement_learning/q_learning/4-play.py
+++ reinforcement_learning/q_learning/4-play.py
@@ -10,17 +10,17 @@ def play(env, Q, max_steps=100):
         max_steps: the maximum number of steps in the episode
         Returns: the total rewards for the episode
     """
-    state = env.reset()
+    state, _ = env.reset()
     done = False
-    total_rewards = 0
+    total_reward = 0
     rendered_outputs = []
     rendered_outputs.append(env.render())
     for step in range(max_steps):
         action = np.argmax(Q[state, :])
         new_state, reward, done, truncated, _ = env.step(action)
-        total_rewards += reward
         rendered_outputs.append(env.render())
         state = new_state
+        total_reward += reward
         if done or truncated:
             break
-    return total_rewards, rendered_outputs
+    return total_reward, rendered_outputs
