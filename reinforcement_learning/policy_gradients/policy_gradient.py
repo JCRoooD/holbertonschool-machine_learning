21b454ffc51b4cdcd7f2ae3d0586d888eb1a72c8
JCRoooD <juancarlos-99@live.com>
2024-11-17 17:05:23 -0400

    task 1 complete

--- reinforcement_learning/policy_gradients/policy_gradient.py
+++ reinforcement_learning/policy_gradients/policy_gradient.py
@@ -11,3 +11,17 @@ def policy(matrix, weight):
     weighted_states = (matrix @ weight)
     e_x = np.exp(weighted_states - np.max(weighted_states))
     return e_x / np.sum(e_x)
+
+
+def policy_gradient(state, weight):
+    """
+    Computes the Monte-Carlo policy gradient based on a state and a
+    weight matrix.
+    """
+    action_probs = policy(state, weight)
+    action = np.random.choice(len(action_probs), p=action_probs)
+    d_softmax = action_probs.copy()
+    d_softmax[action] -= 1
+    grad = -np.outer(state, d_softmax)
+
+    return action, grad
