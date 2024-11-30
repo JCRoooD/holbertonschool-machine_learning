364d5fdec100705a40c13d7efd4ac75c56f3376f
JCRoooD <juancarlos-99@live.com>
2024-11-29 20:47:31 -0400

    hierarchy fix

--- pipeline/pandas/12-hierarchy.py
+++ pipeline/pandas/12-hierarchy.py
@@ -5,16 +5,28 @@ index = __import__('10-index').index
 
 
 def hierarchy(df1, df2):
-    """ function that sets the index of the pd.DataFrame"""
+    """
+    This function sets the index of a dataframe to a hierarchical index
+    Args:
+        df1 (pd.DataFrame): The first DataFrame to index.
+        df2 (pd.DataFrame): The second DataFrame to index.
+    Returns:
+        pd.DataFrame: The filtered, concatenated DataFrame.
+    """
+
+    # Index both DataFrames on their Timestamp column
     df1 = index(df1)
     df2 = index(df2)
 
-    # Concatenate the two pd.DataFrames
-    df1 = df1.loc[:1417411980:1417417980]
-    df2 = df2.loc[:1417411980:1417417980]
+    # Filter the DataFrames for the specified Timestamp range
+    df1 = df1.loc[1417411980:1417417980]
+    df2 = df2.loc[1417411980:1417417980]
 
-    df = pd.concat([df2, df1], keys=['bitstamp', 'coinbase'])
+    # NOTE concatenated dataframes, using keys to differentiate data origin
+    df = pd.concat([df2, df1], keys=["bitstamp", "coinbase"])
 
+    # Reorder index levels to make Timestamp the topmost (leftmost) level
     df = df.reorder_levels([1, 0], axis=0)
 
+    # Sort by chronological order
     return df.sort_index()
