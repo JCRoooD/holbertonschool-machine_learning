b55d49859c07e1feb066289f7b41d8a962621e4d
JCRoooD <juancarlos-99@live.com>
2024-11-29 20:46:15 -0400

    fix errrors

--- pipeline/pandas/5-slice.py
+++ pipeline/pandas/5-slice.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
-""" slice module to slice a pd.DataFrame"""
+"""This module slices a dataframe along the
+columns High and Close"""
 
 
-def slice_data(df):
-    """ function that slices a pd.DataFrame"""
-    return df[[ 'High', 'Low', 'Close', 'Volume_(BTC)']].iloc[::60]
+def slice(df):
+    """This function slices a dataframe along the
+    columns High and Close.
+
+    Args:
+        df: the dataframe to slice
+
+    Returns:
+        The sliced dataframe
+    """
+    # Return the columns 'High' and 'Close'
+    # iloc[::60] is used to slice the dataframe every 60 rows
+    return df[['High', 'Low', 'Close', 'Volume_(BTC)']].iloc[::60]
